a4755941d48d2ce23fedcf2315ab78275a31ad47
Miha <stele1000@gmail.com>
2025-06-29 20:57:21 +0200

    Add basic event management pages and models

---
+++ Models/Event.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorIndependetWebAssemblyNoAndroid.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
